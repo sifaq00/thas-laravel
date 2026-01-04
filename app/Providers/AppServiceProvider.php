9aba82cc44067ff66ee4bbf371c82fe4e1bd8acb
sifaq00 <asysyifaq25@gmail.com>
2026-01-04 18:14:41 +0700

    Force HTTPS and trust proxies for Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Filament\Facades\Filament;
 use Carbon\Carbon;
 
@@ -22,6 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+    // Force HTTPS in production
+    if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
+
     config(['app.timezone' => 'Asia/Jakarta']);
     date_default_timezone_set('Asia/Jakarta');
     Carbon::setLocale('id');
