61ea33f84aad7574e9c68cbfb48e1b83b17d3643
sifaq00 <asysyifaq25@gmail.com>
2026-01-04 23:03:07 +0700

    edit image route

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -19,4 +19,15 @@ class Article extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function getImageUrlAttribute()
+    {
+        if (!$this->image) return null;
+
+        return 'https://res.cloudinary.com/' .
+            config('cloudinary.cloud_name') .
+            '/image/upload/' .
+            $this->image;
+    }
+
 }
