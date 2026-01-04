3f3b747b9f5d9e2bd5a76b1754ae89c1f6df0bb1
sifaq00 <asysyifaq25@gmail.com>
2026-01-04 19:23:32 +0700

    fix mixed content vite https

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: '/',
     plugins: [
         laravel({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/filament/admin/theme.css'
+                'resources/css/filament/admin/theme.css',
             ],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(),
     ],
     resolve: {
         alias: {
